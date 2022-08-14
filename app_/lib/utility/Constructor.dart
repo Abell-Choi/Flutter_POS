8156ffa3ed84d38ce593e36b76e167eedee800f9
Abell-Choi <trooperalex92@gmail.com>
2022-08-14 13:59:36 +0900

    add LogPreset class

--- app_/lib/utility/Constructor.dart
+++ app_/lib/utility/Constructor.dart
@@ -1,13 +1,67 @@
-import 'dart:ffi';
+import 'dart:convert';
 
 import 'package:intl/intl.dart'; 
 class OptionData{
 }
 
 class LogPreset{
-  DateTime _dateTime;
-  List<GoodsPreset> _sellList ;
-  LogPreset(DateTime this._dateTime, List<GoodsPreset> this._sellList);
+  DateTime? updateTime;
+  String? uuid;
+  List<GoodsPreset>? goodsPresets;
+  List<String> _defaultKeyList = ['updateTime', 'uuid', 'goodsPresets'];
+
+  LogPreset(String this.uuid, List<GoodsPreset> this.goodsPresets){
+    this.updateTime = DateTime.now();
+  }
+
+  //MapData로 반환 (Stringify 화)
+  Map<String, dynamic> getMapData(){  
+    Map<String ,dynamic> _data = {
+      'updateTime' : this.updateTime.toString(),
+      'uuid' : this.uuid
+    };
+
+    List<String> goodsLists = [];
+    if (goodsPresets == null) { 
+      _data['goodsPresets']=[];
+      Map<String, dynamic> _resultData = {
+        this.uuid! : Map.from(_data)
+      };
+      return _resultData;
+    }
+    else{
+      for (var i in this.goodsPresets!){
+        goodsLists.add(jsonEncode(i!.getMapData()));
+      }
+    }
+
+    _data['goodsPresets'] = goodsLists;
+    return {this.uuid! : Map.from(_data)};
+  }
+
+  //MapData -> Class 변환
+  bool convertMapToClass(Map<String ,dynamic> data){
+    //uuid:{updatetime, uuid, goodsPresets}
+    for (String i in this._defaultKeyList){
+      if (data.keys.toList().indexOf(i) == -1){
+        return false;
+      }
+    }
+
+    if (DateTime.tryParse(data['updateTime']) == null){
+      return false;
+    } this.updateTime = DateTime.tryParse(data['updateTime'])!; 
+
+    this.uuid = data['uuid'];
+    this.goodsPresets = <GoodsPreset>[];
+    for (var i in data['goodsPresets']){
+      if (i == null) { this.goodsPresets = null; return true; }
+      GoodsPreset _convertData = GoodsPreset();
+      _convertData.convertMapToClass(i);
+      this.goodsPresets!.add(_convertData);
+    }
+    return true;
+  }
 }
 
 class GoodsPreset{
