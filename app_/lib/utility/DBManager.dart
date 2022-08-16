6e878c013efabc45e0c3b4e0787ed5c9ce242a1e
Abell-Choi <trooperalex92@gmail.com>
2022-08-16 15:06:30 +0900

    making DBManager.

--- app_/lib/utility/DBManager.dart
+++ app_/lib/utility/DBManager.dart
@@ -1,4 +1,11 @@
 class DBManager{
-  Stirng url;
-  String 
+  String url;
+  String id;
+  String passwd;
+
+  DBManager(String this.url, String this.id, String this.passwd);
+
+  Future<Map<String, dynamic>> connect() async {
+    return {};
+  }
 }
